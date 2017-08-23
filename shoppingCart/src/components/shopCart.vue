fa2fa4d45d5197297912d7d795febc49544e7127
libin <1078795691@qq.com>
2017-08-23 23:16:22 +0800

    两个空白页面

---
+++ shoppingCart/src/components/shopCart.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <router-link to='/'>go to goods</router-link>
+    this is shopCart
+  </div>
+</template>
+
+<script></script>
+
+<style>
+</style>
--- shoppingCart/src/router/index.js
+++ shoppingCart/src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import goods from '@/components/goods'
 
 Vue.use(Router)
+
+import shopCart from '@/components/shopCart.vue'
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      component: goods
+    },
+    {
+      path: '/shopCart',
+      component: shopCart
     }
   ]
 })
